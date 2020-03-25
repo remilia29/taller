a88efd9df5151a830642386efc4a86c28afd2857
Patty <polivares29@alumnos.uaq.mx>
2020-03-25 15:14:34 -0600

    pokedex

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const express = require('express');
 const app = express();
 const{pokemon} = require ('./pokedex.json');
 
-app.get('/', (req,res,next) => {
+app.get("/", (req,res,next) => {
     res.status(200);
-    res.send("bienvenido al poquedex");
+    res.send("bienvenido al pokdex");
 });
 
-app.get('/pokemon', (req,res,next)=>{
-   res.status(200)
+app.get('/pokemon/all', (req,res,next)=>{
+    res.status(200);
     res.send(pokemon);
 });
 
-app.get('/poquemon/:id',(req,res,next)=> {
+app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=> {
+    const id = req.params.id - 1;
+    if (id >= 0 && id <= 150) {
     res.status(200);
-    res.send (pokemon [req.params.id - 1]);
+    return res.send(pokemon [req.params.id - 1]);
+    }
+
+    else{
+        res.status(404);
+        res.send ("pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req,res,next)=>{
+const name = req.params.name;
+for (i = 0;i< pokemon.length;i++){
+    if (pokemon[i].name == name) {
+    res.status(200);
+    res.send(pokemon[i]);
+    }
+}
 });
 
 app.listen(process.env.PORT || 3000, () =>{
